f9dddcdd159006ba8228c7925d700a46e7fc64d2
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-11-04 20:22:32 +0200

    Add Same function with comparable constraint

--- generics/generics.go
+++ generics/generics.go
@@ -10,10 +10,19 @@ func PrintSlice[T any](s []T) {
 	}
 }
 
+// Same returns true if the two provided values are equal, and false otherwise.
+// The function accepts any comparable type, allowing comparisons between values
+// of the same type, such as integers, strings, or custom comparable types.
+func Same[T comparable](a, b T) bool {
+	return a == b
+}
+
 func main() {
 	xi := []int{64, 128, 256, 512}
 	xs := []string{"Server uptime: 152 days, 6 hours, 42 minutes.", "Database connection established on port 5432."}
 
 	PrintSlice(xi)
 	PrintSlice(xs)
+
+	fmt.Println(Same([3]int{1, 2, 99}, [3]int{1, 2, 3})) // You are allowed to compare two arrays
 }
