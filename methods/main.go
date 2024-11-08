76215f593996fe5d65b5f18c4b59fb7e1b4c4626
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-11-08 15:15:45 +0200

    [METHODS] Add type method implementation

---
+++ methods/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type ar2x2 [2][2]int
+
+// Traditional Add() function
+func Add(a, b ar2x2) ar2x2 {
+	c := ar2x2{}
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			c[i][j] = a[i][j] + b[i][j]
+			fmt.Printf("%d = %d + %d\n", c[i][j], a[i][j], b[i][j])
+		}
+	}
+
+	return c
+}
+
+// Type method Add()
+func (a *ar2x2) Add(b ar2x2) {
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 2; j++ {
+			a[i][j] = a[i][j] + b[i][j]
+		}
+	}
+}
+
+func main() {
+	ax := ar2x2{[2]int{1, 2}, [2]int{3, 4}}
+	ay := ar2x2{[2]int{5, 6}, [2]int{7, 8}}
+
+	res := Add(ax, ay)
+	fmt.Printf("res: %v\n", res)
+
+	ax.Add(ay)
+	fmt.Printf("ax: %v\n", ax)
+}
