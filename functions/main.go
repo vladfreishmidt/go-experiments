6b92fd1a89af98ec676e75c0ae54a56519f79144
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-11-14 15:05:55 +0200

    [Functions] Add named return values

--- functions/main.go
+++ functions/main.go
@@ -16,4 +16,27 @@ func main() {
 		return param * param
 	}
 	fmt.Println("anF of", n, "is", anF(n))
+	fmt.Println(sortTwo(1, -3))
+	fmt.Println(sortTwo(-1, 0))
+
+}
+
+func sortTwo(x, y int) (int, int) {
+	if x > y {
+		return y, x
+	}
+	return x, y
+}
+
+// Named return values
+func minMax(x, y int) (min, max int) {
+	if x > y {
+		min = y
+		max = x
+		return min, max
+	}
+
+	min = x
+	max = y
+	return
 }
