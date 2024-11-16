fa51ab71d06941656849682d6c5d8ac7d039c606
Vlad Freishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-11-16 18:54:38 +0200

    [Functions] Add funRet example

--- functions/main.go
+++ functions/main.go
@@ -67,3 +67,16 @@ func minMax(x, y int) (min, max int) {
 	max = y
 	return
 }
+
+func funRet(i int) func(int) int {
+	if i < 0 {
+		return func(k int) int {
+			k = -k
+			return k + k
+		}
+	}
+
+	return func(k int) int {
+		return k * k
+	}
+}
